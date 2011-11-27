c314f6f6eb9336bac7079afe56ee804c45a5a023
nas774 <nas7743@gmail.com>
2011-11-27 15:03:18 +0900

    * new command line splitter * "dc" command supports issuing by query. * add "dcl" command to discoveyCall by location for ease. * extended "call" command to call by an agentid and an agentid with peerid. * support call/discoveryCall|Oneway|Async.

--- src/org/github/nas774/piax/piaxshell/PIAXShell.java
+++ src/org/github/nas774/piax/piaxshell/PIAXShell.java
@@ -17,12 +17,17 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Properties;
 import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.piax.agent.AgentHome;
 import org.piax.agent.AgentId;
 import org.piax.agent.AgentPeer;
 import org.piax.agent.NoSuchAgentException;
+import org.piax.trans.common.FutureReturn;
+import org.piax.trans.common.PeerId;
 import org.piax.trans.common.PeerLocator;
 import org.piax.trans.common.ReturnSet;
 import org.piax.trans.ts.tcp.TcpLocator;
@@ -49,6 +54,7 @@ public class PIAXShell {
     private PeerLocator myLocator = null;
     private Collection<PeerLocator> seeds = null;
     private File[] agclassesdirs = null;
+    private ExecutorService asyncReturnExecutor = Executors.newCachedThreadPool();
 
     private boolean autojoin = DEFAULT_PEER_AUTOJOIN;
 
@@ -89,7 +95,7 @@ public class PIAXShell {
      * 
      * @param args
      *            command line argument.
-     * @return true:success false:false, maybe some errors occuered.
+     * @return true:success false:fail, maybe some errors occured.
      */
     protected boolean initSettings(String args[]) {
         try {
@@ -502,22 +508,58 @@ public class PIAXShell {
                         + "  sl)eep agent_NO sleep agent indicated by agent_NO\n"
                         + "  wa)ke agetn_NO  wakeup agent indicated by agent_NO\n"
                         + "  fin agent_NO    destroy agent indicated by agent_NO\n"
-                        + "  dc,discover method arg ...\n"
-                        + "                  discoveryCall to agents in whole area\n"
-                        + "  \n" + "  c)all agent_NO method arg ...\n"
-                        + "                  call agent method\n" + "  \n"
+                        + "  \n"
+                        + "  c)all agent_NO method arg ...\n"
+                        + "  c)all aid:agent_ID method arg ...\n"
+                        + "  c)all pid:peer_ID aid:agent_ID method arg ...\n"
+                        + "                  call remote agent method\n" + "  \n"
+                        + "  co,calloneway agent_NO method arg ...\n"
+                        + "  co,calloneway aid:agent_ID method arg ...\n"
+                        + "  co,calloneway pid:peer_ID aid:agent_ID method arg ...\n"
+                        + "                  callOneway remote agent method\n" + "  \n"
+                        + "  ca,callasync agent_NO method arg ...\n"
+                        + "  ca,callasync aid:agent_ID method arg ...\n"
+                        + "  ca,callasync pid:peer_ID aid:agent_ID method arg ...\n"
+                        + "                  callAsync remote agent method\n" + "  \n"
+                        + "  \n"
+                        + "  dc,discover query method arg ...\n"
+                        + "                  discoveryCall to agents by query\n"
+                        + "  dco,discoveroneway query method arg ...\n"
+                        + "                  discoveryCallOneway to agents by query\n"
+                        + "  dca,discoverasync query method arg ...\n"
+                        + "                  discoveryCallAsync to agents by query\n"
+                        + "  dcl,discoverlocation lng lat w h method arg ...\n"
+                        + "                  discoveryCall to agents in (lng, lat, w, h) area\n"
                         + "  ?,help          show this help message\n"
                         + "  bye             exit\n" + "\n");
     }
 
-    private String[] split(String line) {
-        ArrayList<String> lines = new ArrayList<String>();
-        StringTokenizer st = new StringTokenizer(line);
-        while (st.hasMoreTokens()) {
-            lines.add(st.nextToken());
+    static Pattern cmd_arg_divide = Pattern.compile("([a-zA-Z][\\w-]+)(\\s.+)?");
+    static Pattern arg_parse = Pattern.compile("\\s+(?:(?:\"((?:(?:\\\\\")|[^\"])*)\"?)|([^\"]\\S*))");
+    private static String[] split(String line) {
+        line = line.trim();
+        ArrayList<String> cmd_args = new ArrayList<String>();
+        Matcher m = cmd_arg_divide.matcher(line);
+        if (!m.matches())
+            return null;
+        String cmd = m.group(1);
+        String arg = m.group(2);
+
+        cmd_args.add(cmd);
+
+        if (arg == null)
+            return cmd_args.toArray(new String[cmd_args.size()]);
+
+        m = arg_parse.matcher(arg);
+        while (m.find()) {
+            for (int i=1 ;i<m.groupCount()+1;i++) {
+                if (m.group(i) != null) {
+                    cmd_args.add(m.group(i).replaceAll("\\\\", ""));
+                    break;
+                }
+            }
         }
-        String[] rlines = new String[lines.size()];
-        return (String[]) lines.toArray(rlines);
+        return cmd_args.toArray(new String[cmd_args.size()]);
     }
 
     private List<AgentId> agents;
@@ -609,26 +651,108 @@ public class PIAXShell {
                         continue;
                     }
                     fin(Integer.parseInt(args[1]));
-                } else if (args[0].equals("discover") || args[0].equals("dc")) {
-                    if (args.length < 2) {
-                        printHelp();
-                        continue;
-                    }
-                    Object[] cargs = new String[args.length - 2];
-                    for (int i = 0; i < cargs.length; i++) {
-                        cargs[i] = args[i + 2];
-                    }
-                    discover(args[1], cargs);
-                } else if (args[0].equals("call") || args[0].equals("c")) {
+                } else if (args[0].equals("discover") || args[0].equals("dc") ||
+                           args[0].equals("discoveroneway") || args[0].equals("dco") ||
+                           args[0].equals("discoverasync") || args[0].equals("dca")) {
                     if (args.length < 3) {
                         printHelp();
                         continue;
                     }
-                    Object[] cargs = new String[args.length - 3];
-                    for (int i = 0; i < cargs.length; i++) {
-                        cargs[i] = args[i + 3];
+                    String query = args[1];
+                    String method = args[2];
+                    Object[] dcargs = new String[args.length - 3];
+                    for (int i = 0; i < dcargs.length; i++) {
+                        dcargs[i] = args[i + 3];
                     }
-                    call(Integer.parseInt(args[1]), args[2], cargs);
+                    if (args[0].equals("discover") || args[0].equals("dc")) {
+                        discover(query, method, dcargs);
+                    } else if (args[0].equals("discoveroneway") || args[0].equals("dco")) {
+                        discoverOneway(query, method, dcargs);
+                    } else if (args[0].equals("discoverasync") || args[0].equals("dca")) {
+                        discoverAsync(query, method, dcargs);
+                    }
+                } else if (args[0].equals("discoverlocation") || args[0].equals("dcl")) {
+                    if (args.length < 6) {
+                        printHelp();
+                        continue;
+                    }
+                    double lng = Double.parseDouble(args[1]);
+                    double lat = Double.parseDouble(args[2]);
+                    double w = Double.parseDouble(args[3]);
+                    double h = Double.parseDouble(args[4]);
+                    String method = args[5];
+                    Object[] dcargs = new String[args.length - 6];
+                    for (int i = 0; i < dcargs.length; i++) {
+                        dcargs[i] = args[i + 6];
+                    }
+                    discoverLocation(lng, lat, w, h, method, dcargs);
+                } else if (args[0].equals("call") || args[0].equals("c") ||
+                           args[0].equals("calloneway") || args[0].equals("co") ||
+                           args[0].equals("callasync") || args[0].equals("ca")) {
+                    if (args.length < 3) {
+                        printHelp();
+                        continue;
+                    }
+                    int calltype = 0;
+                    if (args[0].equals("call") || args[0].equals("c")) {
+                        calltype = 0;
+                    } else if (args[0].equals("calloneway") || args[0].equals("co")) {
+                        calltype = 1;
+                    } else if (args[0].equals("callasync") || args[0].equals("ca")) {
+                        calltype = 2;
+                    }
+
+                    int agno = -1;
+                    try {
+                        agno = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException e) {
+                    }
+                    if (agno != -1) {
+                        String method = args[2];
+                        Object[] cargs = new String[args.length - 3];
+                        for (int i = 0; i < cargs.length; i++) {
+                            cargs[i] = args[i + 3];
+                        }
+                        if (calltype == 0) {
+                            call(agno, method, cargs);
+                        } else if (calltype == 1) {
+                            callOneway(agno, method, cargs);
+                        } else if (calltype == 2) {
+                            callAsync(agno, method, cargs);
+                        }
+                    } else if (args[1].toLowerCase().startsWith("aid:")) {
+                        AgentId agid = new AgentId(args[2].substring(4));
+                        String method = args[2];
+                        Object[] cargs = new String[args.length - 3];
+                        for (int i = 0; i < cargs.length; i++) {
+                            cargs[i] = args[i + 3];
+                        }
+                        if (calltype == 0) {
+                            call(agid, method, cargs);
+                        } else if (calltype == 1) {
+                            callOneway(agid, method, cargs);
+                        } else if (calltype == 2) {
+                            callAsync(agid, method, cargs);
+                        }
+                    } else if (args.length < 4 && args[1].toLowerCase().startsWith("pid:") && args[2].toLowerCase().startsWith("aid:")) {
+                        PeerId pid = new PeerId(args[1].substring(4));
+                        AgentId agid = new AgentId(args[2].substring(4));
+                        String method = args[3];
+                        Object[] cargs = new String[args.length - 4];
+                        for (int i = 0; i < cargs.length; i++) {
+                            cargs[i] = args[i + 4];
+                        }
+                        if (calltype == 0) {
+                            call(pid, agid, method, cargs);
+                        } else if (calltype == 1) {
+                            callOneway(pid, agid, method, cargs);
+                        } else if (calltype == 2) {
+                            callAsync(pid, agid, method, cargs);
+                        }
+                    } else {
+                        printHelp();
+                        continue;
+                    }
                 } else if (args[0].equals("help") || args[0].equals("?")) {
                     printHelp();
                     continue;
@@ -793,11 +917,85 @@ public class PIAXShell {
         }
     }
 
-    void discover(String method, Object... args) {
+    void discover(String query, String method, Object... args) {
         ReturnSet<Object> rset = null;
         try {
-            rset = peer.getHome().discoveryCallAsync(
-                    "location in rect(0,0,1,1)", method, args);
+            rset = peer.getHome().discoveryCallAsync(query, method, args);
+        } catch (IllegalStateException e) {
+            System.out.println("\t>> not joined.");
+            return;
+        } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+            System.out.println("\t>> could not discovery call.");
+            return;
+        }
+
+        while (rset.hasNext()) {
+            Object value;
+            try {
+                value = rset.getNext(3000);
+            } catch (InterruptedException e) {
+                break;
+            } catch (NoSuchElementException e) {
+                break;
+            } catch (InvocationTargetException e) {
+                continue;
+            }
+            long etime = System.currentTimeMillis();
+            System.out.println(" value: " + value);
+            System.out.println(" peerId: " + rset.getThisPeerId());
+            AgentId agId = (AgentId) rset.getThisTargetId();
+            System.out.println(" agentId: " + agId);
+            System.out.println("\t## time (msec): " + (etime - stime));
+        }
+    }
+
+    void discoverAsync(String query, String method, Object... args) {
+        try {
+            final ReturnSet<Object> rset = peer.getHome().discoveryCallAsync(query, method, args);
+            asyncReturnExecutor.execute(new Runnable() {
+                public void run() {
+                    while (rset.hasNext()) {
+                        Object value;
+                        try {
+                            value = rset.getNext(3000);
+                        } catch (InterruptedException e) {
+                            break;
+                        } catch (NoSuchElementException e) {
+                            break;
+                        } catch (InvocationTargetException e) {
+                            continue;
+                        }
+                        long etime = System.currentTimeMillis();
+                        System.out.println(" value: " + value);
+                        System.out.println(" peerId: " + rset.getThisPeerId());
+                        AgentId agId = (AgentId) rset.getThisTargetId();
+                        System.out.println(" agentId: " + agId);
+                        System.out.println("\t## time (msec): " + (etime - stime));
+                    }
+                }
+            });
+        } catch (IllegalStateException e) {
+            System.out.println("\t>> not joined.");
+            return;
+        } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+            System.out.println("\t>> could not discovery call.");
+            return;
+        }
+    }
+
+    void discoverOneway(String query, String method, Object... args) {
+        peer.getHome().discoveryCallOneway(query, method, args);
+    }
+
+    void discoverLocation(double lng, double lat, double w, double h, String method, Object... args) {
+        ReturnSet<Object> rset = null;
+        try {
+            String query = String.format("%s inside rect(%f, %f, %f, %f)", 
+                    org.piax.agent.ConfigValues.LOCATION_ATTRIB_NAME,
+                    lng, lat, w, h);
+            rset = peer.getHome().discoveryCallAsync(query, method, args);
         } catch (IllegalStateException e) {
             System.out.println("\t>> not joined.");
             return;
@@ -833,9 +1031,87 @@ public class PIAXShell {
             return;
         }
         AgentId agId = agents.get(agentNo);
+        call(agId, method, cargs);
+    }
+
+    void call(AgentId agid, String method, Object... cargs) {
+        call(null, agid, method, cargs);
+    }
+
+    void call(PeerId pid, AgentId agid, String method, Object... cargs) {
         try {
-            Object obj = peer.getHome().call(agId, method, cargs);
+            Object obj = null;
+            if (pid == null) {
+                obj = peer.getHome().call(agid, method, cargs);
+            } else {
+                obj = peer.getHome().call(pid, agid, method, cargs);
+            }
             System.out.println(" return value: " + obj);
+        } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+            System.out.println("\t>> cannot call agent.");
+        }
+    }
+
+    void callAsync(int agentNo, String method, Object... cargs) {
+        if (agents.size() <= agentNo) {
+            System.out.println("\t>> invalid agent NO.");
+            return;
+        }
+        AgentId agId = agents.get(agentNo);
+        callAsync(agId, method, cargs);
+    }
+
+    void callAsync(AgentId agid, String method, Object... cargs) {
+        callAsync(null, agid, method, cargs);
+    }
+
+    void callAsync(PeerId pid, AgentId agid, String method, Object... cargs) {
+        try {
+            final FutureReturn<Object> future;
+            if (pid == null) {
+                future = peer.getHome().callAsync(agid, method, cargs);
+            } else {
+                future = peer.getHome().callAsync(pid, agid, method, cargs);
+            }
+            asyncReturnExecutor.execute(new Runnable() {
+                public void run() {
+                    try {
+                        Object obj = future.get();
+                        System.out.println(" return value: " + obj);
+                    } catch (Exception e) {
+                        logger.warn(e.getMessage(), e);
+                        System.out.println("\t>> cannot call agent.");
+                    }
+                }
+            });
+
+        } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+            System.out.println("\t>> cannot call agent.");
+        }
+    }
+
+    void callOneway(int agentNo, String method, Object... cargs) {
+        if (agents.size() <= agentNo) {
+            System.out.println("\t>> invalid agent NO.");
+            return;
+        }
+        AgentId agId = agents.get(agentNo);
+        callOneway(agId, method, cargs);
+    }
+
+    void callOneway(AgentId agid, String method, Object... cargs) {
+        callOneway(null, agid, method, cargs);
+    }
+
+    void callOneway(PeerId pid, AgentId agid, String method, Object... cargs) {
+        try {
+            if (pid == null) {
+                peer.getHome().callOneway(agid, method, cargs);
+            } else {
+                peer.getHome().callOneway(pid, agid, method, cargs);
+            }
         } catch (Exception e) {
             logger.warn(e.getMessage(), e);
             System.out.println("\t>> cannot call agent.");
